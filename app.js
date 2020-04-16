43b87800581c4d3d4931c5c2551c32ba07eb5b1e
navneeeth <navneeth.padaki15@gmail.com>
2020-04-16 16:05:20 +0530

    4) The Global Object.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hey ninjas!');
+var time = 0;
+
+var timer = setInterval(function() {
+  time += 2;
+  console.log(time + ' seconds have passed.');
+  if(time > 5) {
+    clearInterval(timer);
+  }
+}, 2000);
+
+console.log(__dirname);
+console.log(__filename);
