cbabe130a2b527558a1b3f4708d5f690b26830c3
navneeeth <navneeth.padaki15@gmail.com>
2020-04-16 16:09:01 +0530

    5) Function Expressions.

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-var time = 0;
+// var time = 0;
+//
+// var timer = setInterval(function() {
+//   time += 2;
+//   console.log(time + ' seconds have passed.');
+//   if(time > 5) {
+//     clearInterval(timer);
+//   }
+// }, 2000);
+//
+// console.log(__dirname);
+// console.log(__filename);
 
-var timer = setInterval(function() {
-  time += 2;
-  console.log(time + ' seconds have passed.');
-  if(time > 5) {
-    clearInterval(timer);
-  }
-}, 2000);
+//normal function statement
+// function sayHi(){
+//   console.log('Hi');
+// }
+//
+// sayHi();
 
-console.log(__dirname);
-console.log(__filename);
+//function expresssion
+var sayBye = function(){
+  console.log('Bye');
+};
+sayBye();
+
+function callFunction(fun) {
+  fun();
+}
+
+callFunction(sayBye);
