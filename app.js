9bb91c63b85ac175a4f5292f7559c244d3261f56
navneeeth <navneeth.padaki15@gmail.com>
2020-04-17 19:52:50 +0530

    8) The Node Event Emitter.

--- app.js
+++ app.js
@@ -30,8 +30,40 @@
 //
 // callFunction(sayBye);
 
-var stuff = require('./stuff');
+// var stuff = require('./stuff');
 
-console.log(stuff.counter(['shaun', 'crystal', 'ryu']));
-console.log(stuff.adder(5, 6));
-console.log(stuff.adder(stuff.pi, 5));
+// console.log(stuff.counter(['shaun', 'crystal', 'ryu']));
+// console.log(stuff.adder(5, 6));
+// console.log(stuff.adder(stuff.pi, 5));
+
+var events = require('events');
+
+// var myEmitter = new events.EventEmitter();
+//
+// myEmitter.on('someEvent', function(msg) {
+//   console.log(msg);
+// });
+//
+// myEmitter.emit('someEvent', 'the event was emitted');
+
+var util = require('util');
+
+
+var Person = function(name) {
+  this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person('James');
+var mary = new Person('Mary');
+var ryu = new Person('Ryu');
+var people =[james, mary, ryu];
+people.forEach(function(person){
+  person.on('speak', function(msg){
+    console.log(person.name + ' said: '+msg);
+  });
+});
+
+james.emit('speak', 'hey dudes');
+ryu.emit('speak', 'I want a curry');
