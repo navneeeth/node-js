f474ab90a7ee3d76a1def7711822925bc9527e8a
navneeeth <navneeth.padaki15@gmail.com>
2020-05-08 01:44:29 +0530

    19) Basic Routing.

--- app.js
+++ app.js
@@ -155,17 +155,41 @@
 // console.log("Yo dawgs, now listening to port 3000");
 
 //18 Serving JSON data
+// var http = require('http');
+// var fs = require('fs');
+// var server = http.createServer(function(req, res) {
+//   console.log('Request was made: ' + req.url);
+//   res.writeHead(200, {'Content-Type': 'application/json'});
+//   var myObj = {
+//     name: 'Ryu',
+//     job: 'Ninja',
+//     age: 29
+//   };
+//   res.end(JSON.stringify(myObj));
+// });
+//
+// server.listen(3000, '127.0.0.1');
+// console.log("Yo dawgs, now listening to port 3000");
+
+//19 basic routing
 var http = require('http');
 var fs = require('fs');
 var server = http.createServer(function(req, res) {
   console.log('Request was made: ' + req.url);
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  var myObj = {
-    name: 'Ryu',
-    job: 'Ninja',
-    age: 29
-  };
-  res.end(JSON.stringify(myObj));
+  if(req.url === '/home' || req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+  } else if(req.url === '/contact') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/contact.html').pipe(res);
+  } else if(req.url === '/api/ninjas') {
+    var ninjas = [{name: 'ryu', age: 29}, {name: 'yoshiz', age: 33}];
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(ninjas));
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/404.html').pipe(res);
+  }
 });
 
 server.listen(3000, '127.0.0.1');
