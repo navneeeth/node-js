f45fb8aca8bfa9bd55ab4de91aaeaa7aad72dd5d
navneeeth <navneeth.padaki15@gmail.com>
2020-05-07 23:33:55 +0530

    9) Reading & Writing Files.

--- app.js
+++ app.js
@@ -36,34 +36,48 @@
 // console.log(stuff.adder(5, 6));
 // console.log(stuff.adder(stuff.pi, 5));
 
-var events = require('events');
-
-// var myEmitter = new events.EventEmitter();
+// var events = require('events');
 //
-// myEmitter.on('someEvent', function(msg) {
-//   console.log(msg);
+// // var myEmitter = new events.EventEmitter();
+// //
+// // myEmitter.on('someEvent', function(msg) {
+// //   console.log(msg);
+// // });
+// //
+// // myEmitter.emit('someEvent', 'the event was emitted');
+//
+// var util = require('util');
+//
+//
+// var Person = function(name) {
+//   this.name = name;
+// };
+//
+// util.inherits(Person, events.EventEmitter);
+//
+// var james = new Person('James');
+// var mary = new Person('Mary');
+// var ryu = new Person('Ryu');
+// var people = [james, mary, ryu];
+// people.forEach(function(person){
+//   person.on('speak', function(msg){
+//     console.log(person.name + ' said: '+msg);
+//   });
 // });
 //
-// myEmitter.emit('someEvent', 'the event was emitted');
-
-var util = require('util');
-
-
-var Person = function(name) {
-  this.name = name;
-};
-
-util.inherits(Person, events.EventEmitter);
-
-var james = new Person('James');
-var mary = new Person('Mary');
-var ryu = new Person('Ryu');
-var people =[james, mary, ryu];
-people.forEach(function(person){
-  person.on('speak', function(msg){
-    console.log(person.name + ' said: '+msg);
+// james.emit('speak', 'hey dudes');
+// ryu.emit('speak', 'I want a curry');
+//reading from a file synchronously.
+var fs = require('fs');
+var readMe = fs.readFileSync('readme.txt', 'utf8');
+//the readFileSync method does not proceed until read is complete.
+console.log(readMe);
+//writing to a file synchronously.
+fs.writeFileSync('writeme.txt', readMe);
+//reading and writing asynchronously.
+fs.readFile('readme.txt', 'utf8', function(err, data) {
+  //console.log(data);
+  fs.writeFile('writeme2.txt', data, function(err, data) {
+    console.log(data);
   });
 });
-
-james.emit('speak', 'hey dudes');
-ryu.emit('speak', 'I want a curry');
