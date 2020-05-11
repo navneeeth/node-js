1ead25ad9fbb8c426cd734f7857bc3a5eab5ccd7
navneeeth <navneeth.padaki15@gmail.com>
2020-05-11 20:23:24 +0530

    24) Express Route Params.

--- app.js
+++ app.js
@@ -204,4 +204,7 @@ app.get('/', function(req, res) {
 app.get('/contact', function(req, res) {
   res.send('This is the contact page');
 });
+app.get('/profile/:id', function(req, res) {
+  res.send('You requested to see a profile with the id of ' + req.params.id);
+});
 app.listen(3000);
