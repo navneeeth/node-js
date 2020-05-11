a63124afe55b09666e66abce40e42edc15f4c34b
navneeeth <navneeth.padaki15@gmail.com>
2020-05-11 20:15:04 +0530

    23) Introduction to Express.

--- app.js
+++ app.js
@@ -172,25 +172,36 @@
 // console.log("Yo dawgs, now listening to port 3000");
 
 //19 basic routing
-var http = require('http');
-var fs = require('fs');
-var server = http.createServer(function(req, res) {
-  console.log('Request was made: ' + req.url);
-  if(req.url === '/home' || req.url === '/') {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/index.html').pipe(res);
-  } else if(req.url === '/contact') {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/contact.html').pipe(res);
-  } else if(req.url === '/api/ninjas') {
-    var ninjas = [{name: 'ryu', age: 29}, {name: 'yoshiz', age: 33}];
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify(ninjas));
-  } else {
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/404.html').pipe(res);
-  }
-});
+// var http = require('http');
+// var fs = require('fs');
+// var server = http.createServer(function(req, res) {
+//   console.log('Request was made: ' + req.url);
+//   if(req.url === '/home' || req.url === '/') {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     fs.createReadStream(__dirname + '/index.html').pipe(res);
+//   } else if(req.url === '/contact-us') {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     fs.createReadStream(__dirname + '/contact.html').pipe(res);
+//   } else if(req.url === '/api/ninjas') {
+//     var ninjas = [{name: 'ryu', age: 29}, {name: 'yoshiz', age: 33}];
+//     res.writeHead(200, {'Content-Type': 'application/json'});
+//     res.end(JSON.stringify(ninjas));
+//   } else {
+//     res.writeHead(404, {'Content-Type': 'text/html'});
+//     fs.createReadStream(__dirname + '/404.html').pipe(res);
+//   }
+// });
+//
+// server.listen(3000, '127.0.0.1');
+// console.log("Yo dawgs, now listening to port 3000");
 
-server.listen(3000, '127.0.0.1');
-console.log("Yo dawgs, now listening to port 3000");
+//23 Introduction to Express
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) {
+  res.send('this is the homepage');
+});
+app.get('/contact', function(req, res) {
+  res.send('This is the contact page');
+});
+app.listen(3000);
